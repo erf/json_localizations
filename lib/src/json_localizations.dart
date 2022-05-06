1a84b1befd1129b03df3f44d7bb6a0ba5b3dcf0e
Erlend Lind Madsen <erlendf80@gmail.com>
2022-05-06 10:58:33 +0200

    json_localizations: docs / refactor

--- lib/src/json_localizations.dart
+++ lib/src/json_localizations.dart
@@ -3,62 +3,60 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-/// Store translations per languageCode/country from a JSON object used by [JsonLocalizationsDelegate].
+/// A class that reads JSON-based localization files.
 class JsonLocalizations {
-  /// Map of translations per language/country code hash.
-  final Map<String, dynamic> _translations = {};
+  /// Translations per language tag.
+  final Map<String, dynamic> translations = {};
 
-  /// Path to translation assets.
+  /// Path to the translation file.
   final String assetPath;
 
   /// The asset bundle.
   final AssetBundle assetBundle;
 
-  /// A hash key of language / country code used for [_translationsMap].
-  late String _langTag;
+  /// A language tag for the current locale.
+  late String langTag;
 
   /// Initialize with asset path to JSON files and an optional assetBundle.
   JsonLocalizations(this.assetPath, [assetBundle])
       : assetBundle = assetBundle ?? rootBundle;
 
-  /// Load and cache a JSON file per language / country code.
-  Future<JsonLocalizations> load(Locale locale) async {
-    // get the key from a combination of languageCode and countryCode
-    _langTag = locale.toLanguageTag();
-
-    // in cache already
-    if (_translations.containsKey(_langTag)) {
-      return this;
-    }
-
-    // load combined key of languageCode and countryCode
+  /// Load a JSON file from the asset bundle and parse it into a map.
+  Future<Map<String, dynamic>> loadFile(Locale locale) async {
     try {
-      final text = await assetBundle.loadString('$assetPath/$_langTag.json');
-      _translations[_langTag] = json.decode(text);
-      return this;
+      final text = await assetBundle.loadString('$assetPath/$langTag.json');
+      return json.decode(text);
     } catch (e) {
       debugPrint(e.toString());
     }
-
     // load only language code
-    if (_langTag != locale.languageCode) {
-      _langTag = locale.languageCode;
+    if (langTag != locale.languageCode) {
+      langTag = locale.languageCode;
       try {
-        final text = await assetBundle.loadString('$assetPath/$_langTag.json');
-        _translations[_langTag] = json.decode(text);
-        return this;
+        final text = await assetBundle.loadString('$assetPath/$langTag.json');
+        return json.decode(text);
       } catch (e) {
         debugPrint(e.toString());
       }
     }
+    // return empty map
+    return {};
+  }
 
-    assert(false, 'Translation file not found for code \'$_langTag\'');
-
+  /// Load and cache a JSON file given a locale.
+  Future<JsonLocalizations> load(Locale locale) async {
+    langTag = locale.toLanguageTag();
+    // in cache already
+    if (translations.containsKey(langTag)) {
+      return this;
+    }
+    // load combined key of languageCode and countryCode
+    translations[langTag] = await loadFile(locale);
     return this;
   }
 
   /// Get translation given a key.
-  dynamic value(String key) => _translations[_langTag]![key];
+  dynamic value(String key) => translations[langTag]![key];
 
   /// Helper for getting [JsonLocalizations] object.
   static JsonLocalizations? of(BuildContext context) =>
